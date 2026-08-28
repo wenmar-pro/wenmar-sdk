3a45a33202fbe5c6684cbcbe04cd6e2047188cff
Ben D'Angelo <ben@bendangelo.me>
2026-08-28 00:52:57 -0400

    feat: require resource wrappers in wenmar.rb

--- ruby/lib/wenmar.rb
+++ ruby/lib/wenmar.rb
@@ -4,6 +4,9 @@ require_relative "wenmar/credentials"
 require_relative "wenmar/client"
 require_relative "wenmar/error"
 require_relative "wenmar/pagination"
+require_relative "wenmar/customer"
+require_relative "wenmar/vehicle"
+require_relative "wenmar/work_order"
 
 module Wenmar
 end
